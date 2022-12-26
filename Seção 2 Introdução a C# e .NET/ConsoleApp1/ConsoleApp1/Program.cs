a3e69df4780621c447d8448ca6b84f8982d4d904
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2022-12-26 08:51:02 -0300

    adição sessao 2

--- Seção 2 Introdução a C# e .NET/ConsoleApp1/ConsoleApp1/Program.cs
+++ Seção 2 Introdução a C# e .NET/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,22 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization; //deixa o ponto como padrão USA
 
-namespace ConsoleApp1
+namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word!");
 
-            SByte x = 100;
+            double x = 10.35784;
+            int y = 32;
+            string z = "Maria";
+            char w = 'F';
+
+            Console.Write("Olá mundo!"); //não realiza a quebra de linha
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Até mais.");
+
+            Console.WriteLine();
             Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2")); //delimita o número de casa pós a vírgula
+            Console.WriteLine(x.ToString("F4")); //delimita o número de casa pós a vírgula
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture)); //além de delimitar, deixa o ponto como padrão USA
 
-            Int16 y = 2093;
-            Console.WriteLine(y);
+            Console.WriteLine();
+            Console.WriteLine("RESULTADO = " + x);
+            Console.WriteLine("O valor do troco é " + x + " reais");
+            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+
+            Console.WriteLine();
+            Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+
+            Console.WriteLine("A paciente {0}, com saldo de {1:F2}, tem {2} anos e seu sexo é: {3}", z, x, y, w); //placeholder
+            //{1:F2} - indica quantas casas decimais pós a vírgula
+
+            Console.WriteLine($"A paciente {z}, com saldo de {x:F2}, tem {y} anos e seu sexo é: {w}");
+
+            Console.WriteLine($"A paciente {z}, com saldo de {x.ToString("F2", CultureInfo.InvariantCulture)}, tem {y} anos e seu sexo é: {w}");
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("-----ENTRADA DE DADOS COM C# ---------");
+
+            
+
+
+
         }
     }
 }
---
+++ Seção 3 Recapitulação de Lógica de Programação usando C#/exer23/exer23/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization; 
+
+namespace exer23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int qntQuartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string line = Console.ReadLine();
+            string[] vet = line.Split(' ');
+
+            Console.WriteLine(nome);
+            Console.WriteLine(qntQuartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(vet[0]);
+            Console.WriteLine(vet[1]);
+            Console.WriteLine(vet[2]);
+
+        }
+    }
+}
